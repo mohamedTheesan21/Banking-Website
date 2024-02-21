e6fe21cc71b2f08ef1f431eb5ba4693f99d90f1f
Theesan <mohamedtheesan4@gmail.com>
2024-02-21 10:04:40 +0530

    Header cmponents created for frontend

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+    return (
+        <div className="header mx-auto mt-0 pb-1 text-center text-bg-primary">
+            <h1>Bank</h1>
+        </div>
+    )
+}
+
+export default Header
