5a4c432fe9df71c13103cc5092e31c1c3dee8497
Theesan <mohamedtheesan4@gmail.com>
2024-03-06 14:12:37 +0530

    Transfer logic added to the backend using mongodb transaction

--- Backend/models/Transfer.js
+++ Backend/models/Transfer.js
@@ -4,7 +4,10 @@ const transferSchema = new mongoose.Schema({
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   amount: Number,
   description: String,
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { 
+    type: String,
+    default: () => new Date().toISOString().split("T")[0] // Function returning default value
+  },
 });
 
 const Transfer = mongoose.model("Transfer", transferSchema);
