c3ded7d2070423e9eeddd949102564cb1fbd8f70
Theesan <mohamedtheesan4@gmail.com>
2024-03-15 20:47:30 +0530

    Pie chart colors changed

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./CSS/Home.css";
-import PieChart from '../Components/PieChart';
+import "./CSS/Home.css"
 
 function Home() {
-    const data = [
-        ["Task", "Hours per Day"],
-        ["Work", 11],
-        ["Eat", 2],
-        ["Commute", 2],
-        ["Watch TV", 2],
-        ["Sleep", 7],
-      ];
+
     return (
         <div className="flex flex-column background-full">
-            <PieChart data={data}/>
             <div className="d-flex button-box m-auto">
                 <a href="/signin" className="btn btn-light">SIGN IN</a>
                 <a href="/contactus" className="btn btn-light">CONTACT US</a>
