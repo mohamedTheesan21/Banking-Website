6bba0d1a5071f35432d295ae5e37fdf481eea9e5
mohamedTheesan21 <129375892+mohamedTheesan21@users.noreply.github.com>
2024-03-16 05:50:12 +0530

    Revert "Master"

--- Backend/models/Verify.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const verifySchema = new mongoose.Schema({
-    email: String,
-    verificationCode: String,
-    timestamp: Number,
-});
-
-module.exports = mongoose.model("Verify", verifySchema);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import "./App.css";
+import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import SignIn from "./Pages/SignIn";
 import Register from "./Pages/Register";
-import Account from "./Pages/Account";
-import Verification from "./Pages/Verification";
-import SignUp from "./Pages/SignUp";
+
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/verification" element={<Verification />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      </Routes><Header />
     </BrowserRouter>
+      
   );
 }
 
